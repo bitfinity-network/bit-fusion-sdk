669d48fa628d9b5f089d3e325a4bc846095ff6f7
Max <maximilian@live.co.uk>
2023-11-30 13:44:03 +0000

    sample dapp

---
+++ dapp/hardhat.config.ts
@@ -0,0 +1,9 @@
+import { ethers } from "hardhat";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+};
+
+export default config;
