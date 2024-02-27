e50fd7f568b12fd8aec616c0e49d2933750e7dc9
Kobby Pentangeli <kobbypentangeli@gmail.com>
2024-02-27 13:08:01 +0000

    Add the `bitcoin_get_current_fee_percentiles` endpoint

--- src/inscription-canister/src/bitcoin_api.rs
+++ src/inscription-canister/src/bitcoin_api.rs
@@ -1,25 +1,24 @@
-use candid::Principal;
-use ic_cdk::api::call::call_with_payment;
-use ic_cdk::api::management_canister::bitcoin::{
-    BitcoinNetwork, GetBalanceRequest, GetUtxosRequest, GetUtxosResponse, Satoshi,
-    SendTransactionRequest,
-};
-
 use crate::{Error, Result};
+use candid::Principal;
+use ic_cdk::api::{
+    call::call_with_payment,
+    management_canister::bitcoin::{
+        BitcoinNetwork, GetBalanceRequest, GetCurrentFeePercentilesRequest, GetUtxosRequest,
+        GetUtxosResponse, MillisatoshiPerByte, Satoshi, SendTransactionRequest,
+    },
+};
 
 // The fees for the various Bitcoin endpoints.
 const GET_BALANCE_COST_CYCLES: u64 = 100_000_000;
 const GET_UTXOS_COST_CYCLES: u64 = 10_000_000_000;
+const GET_CURRENT_FEE_PERCENTILES_CYCLES: u64 = 100_000_000;
 const SEND_TRANSACTION_BASE_CYCLES: u64 = 5_000_000_000;
 const SEND_TRANSACTION_PER_BYTE_CYCLES: u64 = 20_000_000;
 
-#[derive(Debug, Clone)]
-pub enum BitcoinApiError {
-    GetBalanceRequest(String),
-    GetUtxosRequest(String),
-    SendTransactionRequest(String),
-}
-
+/// Returns the balance of the given bitcoin address.
+///
+/// Relies on the `bitcoin_get_balance` endpoint.
+/// See https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-bitcoin_get_balance
 pub async fn get_balance(network: BitcoinNetwork, address: String) -> Result<Satoshi> {
     call_with_payment::<(GetBalanceRequest,), (Satoshi,)>(
         Principal::management_canister(),
@@ -36,6 +35,10 @@ pub async fn get_balance(network: BitcoinNetwork, address: String) -> Result<Sat
     .map(|res| res.0)
 }
 
+/// Returns the UTXOs of the given bitcoin address.
+///
+/// NOTE: Relies on the `bitcoin_get_utxos` endpoint.
+/// See https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-bitcoin_get_utxos
 pub async fn get_utxos(network: BitcoinNetwork, address: String) -> Result<GetUtxosResponse> {
     call_with_payment::<(GetUtxosRequest,), (GetUtxosResponse,)>(
         Principal::management_canister(),
@@ -52,6 +55,29 @@ pub async fn get_utxos(network: BitcoinNetwork, address: String) -> Result<GetUt
     .map(|res| res.0)
 }
 
+/// Returns the 100 fee percentiles measured in millisatoshi/byte.
+/// Percentiles are computed from the last 10,000 transactions (if available).
+///
+/// Relies on the `bitcoin_get_current_fee_percentiles` endpoint.
+/// See https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-bitcoin_get_current_fee_percentiles
+pub async fn get_current_fee_percentiles(
+    network: BitcoinNetwork,
+) -> Result<Vec<MillisatoshiPerByte>> {
+    call_with_payment::<(GetCurrentFeePercentilesRequest,), (Vec<MillisatoshiPerByte>,)>(
+        Principal::management_canister(),
+        "bitcoin_get_current_fee_percentiles",
+        (GetCurrentFeePercentilesRequest { network },),
+        GET_CURRENT_FEE_PERCENTILES_CYCLES,
+    )
+    .await
+    .map_err(|e| Error::CurrentFeePercentilesUnavailable(format!("{:?}", e)))
+    .map(|res| res.0)
+}
+
+/// Sends a (signed) transaction to the bitcoin network.
+///
+/// Relies on the `bitcoin_send_transaction` endpoint.
+/// See https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-bitcoin_send_transaction
 pub async fn send_transaction(network: BitcoinNetwork, transaction: Vec<u8>) -> Result<()> {
     let transaction_fee = SEND_TRANSACTION_BASE_CYCLES
         + (transaction.len() as u64) * SEND_TRANSACTION_PER_BYTE_CYCLES;
