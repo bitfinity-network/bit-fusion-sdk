2a2000a78ccc8b83f327c725c593b1fd43384022
Kobby Pentangeli <kobbypentangeli@gmail.com>
2024-02-28 17:03:16 +0000

    Unified build script

---
+++ scripts/build.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env sh
+
+set -e
+set -x
+
+export RUST_BACKTRACE=full
+
+# Configuration variables
+WASM_DIR=".artifact"
+
+# Initial setup
+initialize_env() {
+    if [ ! -f "./Cargo.toml" ]; then
+        echo "Expecting to run from the cargo root directory, current directory is: $(pwd)"
+        exit 42
+    fi
+
+    if [ "$CI" != "true" ]; then
+        script_dir=$(dirname $0)
+        project_dir=$(realpath "${script_dir}/..")
+
+        echo "Project dir: \"$project_dir\""
+        cd "$project_dir"
+
+        rm -rf "$WASM_DIR"
+        mkdir -p "$WASM_DIR"
+    fi
+}
+
+# Function to build a single canister with a feature flag
+build_canister() {
+    local canister_name="$1"
+    local features="$2"
+    local did_file_name="${3:-$canister_name}"
+
+    echo "Building $canister_name Canister with features: $features"
+
+    # Check for macOS-specific requirements before building
+    if [ "$(uname)" == "Darwin" ]; then
+        LLVM_PATH=$(brew --prefix llvm)
+        # On macOS, use the brew versions of llvm-ar and clang
+        AR="${LLVM_PATH}/bin/llvm-ar" CC="${LLVM_PATH}/bin/clang" cargo build --target wasm32-unknown-unknown --release --package "$canister_name" --features "$features"
+    else
+        cargo build --target wasm32-unknown-unknown --release --package "$canister_name" --features "$features"
+    fi
+
+    ic-wasm "target/wasm32-unknown-unknown/release/$canister_name.wasm" -o "$WASM_DIR/$canister_name.wasm" shrink
+    gzip -k "$WASM_DIR/$canister_name.wasm" --force
+    cargo run > "$WASM_DIR/$canister_name.did"
+}
+
+main() {
+    build_canister "$@"
+}
+
+main "$@"
