6525d640afd70895013df6b211db706750735e74
Kobby Pentangeli <kobbypentangeli@gmail.com>
2024-03-28 12:07:03 +0000

    Use `ic_exports::ic_cdk::api::management_canister::ecdsa` types

--- src/inscriber/src/wallet/ecdsa_api.rs
+++ src/inscriber/src/wallet/ecdsa_api.rs
@@ -1,23 +1,22 @@
-use candid::{CandidType, Principal};
-use ic_exports::ic_cdk;
-use serde::{Deserialize, Serialize};
-
-// The fee for the `sign_with_ecdsa` endpoint using the test key.
-const SIGN_WITH_ECDSA_COST_CYCLES: u64 = 25_000_000_000;
+use candid::CandidType;
+use ic_exports::ic_cdk::api::management_canister::ecdsa::{
+    self as IcEcdsa, EcdsaCurve, EcdsaKeyId, EcdsaPublicKeyArgument, EcdsaPublicKeyResponse,
+    SignWithEcdsaArgument, SignWithEcdsaResponse,
+};
+use serde::Serialize;
 
 /// Retrieves the public key of this canister at the given derivation path
 /// from IC's ECDSA API.
 pub async fn ecdsa_public_key() -> Result<PublicKeyReply, String> {
-    let request = ECDSAPublicKey {
+    let request = EcdsaPublicKeyArgument {
         canister_id: None,
         derivation_path: vec![],
         key_id: EcdsaKeyIds::TestKeyLocalDevelopment.to_key_id(),
     };
 
-    let (res,): (ECDSAPublicKeyReply,) =
-        ic_cdk::call(mgmt_canister_id(), "ecdsa_public_key", (request,))
-            .await
-            .map_err(|e| format!("ecdsa_public_key failed {}", e.1))?;
+    let (res,): (EcdsaPublicKeyResponse,) = IcEcdsa::ecdsa_public_key(request)
+        .await
+        .map_err(|e| format!("ecdsa_public_key failed {}", e.1))?;
 
     Ok(PublicKeyReply {
         public_key_hex: hex::encode(res.public_key),
@@ -26,20 +25,15 @@ pub async fn ecdsa_public_key() -> Result<PublicKeyReply, String> {
 
 /// Signs a message with an ECDSA key and returns the signature.
 pub async fn sign_with_ecdsa(message: &str) -> Result<SignatureReply, String> {
-    let request = SignWithECDSA {
+    let request = SignWithEcdsaArgument {
         message_hash: sha256(message).to_vec(),
         derivation_path: vec![],
         key_id: EcdsaKeyIds::TestKeyLocalDevelopment.to_key_id(),
     };
 
-    let (response,): (SignWithECDSAReply,) = ic_cdk::api::call::call_with_payment(
-        mgmt_canister_id(),
-        "sign_with_ecdsa",
-        (request,),
-        SIGN_WITH_ECDSA_COST_CYCLES,
-    )
-    .await
-    .map_err(|e| format!("sign_with_ecdsa failed {}", e.1))?;
+    let (response,): (SignWithEcdsaResponse,) = IcEcdsa::sign_with_ecdsa(request)
+        .await
+        .map_err(|e| format!("sign_with_ecdsa failed {}", e.1))?;
 
     Ok(SignatureReply {
         signature_hex: hex::encode(response.signature),
@@ -65,10 +59,6 @@ pub async fn verify_ecdsa(
         .is_ok();
 
     Ok(SignatureVerificationReply { is_signature_valid })
-}
-
-fn mgmt_canister_id() -> CanisterId {
-    Principal::management_canister()
 }
 
 fn sha256(input: &str) -> [u8; 32] {
@@ -112,43 +102,4 @@ pub struct SignatureReply {
 #[derive(CandidType, Serialize, Debug)]
 pub struct SignatureVerificationReply {
     pub is_signature_valid: bool,
-}
-
-type CanisterId = Principal;
-
-#[derive(CandidType, Serialize, Debug)]
-struct ECDSAPublicKey {
-    pub canister_id: Option<CanisterId>,
-    pub derivation_path: Vec<Vec<u8>>,
-    pub key_id: EcdsaKeyId,
-}
-
-#[derive(CandidType, Deserialize, Debug)]
-struct ECDSAPublicKeyReply {
-    pub public_key: Vec<u8>,
-    pub chain_code: Vec<u8>,
-}
-
-#[derive(CandidType, Serialize, Debug)]
-struct SignWithECDSA {
-    pub message_hash: Vec<u8>,
-    pub derivation_path: Vec<Vec<u8>>,
-    pub key_id: EcdsaKeyId,
-}
-
-#[derive(CandidType, Deserialize, Debug)]
-struct SignWithECDSAReply {
-    pub signature: Vec<u8>,
-}
-
-#[derive(CandidType, Serialize, Debug, Clone)]
-struct EcdsaKeyId {
-    pub curve: EcdsaCurve,
-    pub name: String,
-}
-
-#[derive(CandidType, Serialize, Debug, Clone)]
-pub enum EcdsaCurve {
-    #[serde(rename = "secp256k1")]
-    Secp256k1,
 }
