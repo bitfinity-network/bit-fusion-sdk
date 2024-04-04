0e06a3b0aaadba9aa5b5d76b77ae1c76ee996736
Andy Osei <anyosei36@gmail.com>
2024-04-04 12:36:31 +0000

    add actor defs for bridge related canisters

--- js-client/src/index.ts
+++ js-client/src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello Word');
+export {
+  ICRC2Minter,
+  createActor as createICRC2MinterActor
+} from './canisters/icrc2-minter';
+
+export { Evm, createActor as createEVMActor } from './canisters/evm';
+
+export {
+  Spender,
+  createActor as createSpenderActor
+} from './canisters/spender';
+
+export { ICRC1, createActor as createICRC1Actor } from './canisters/icrc1';
+
+export {
+  BtcBridge,
+  createActor as createBtcBridgeActor
+} from './canisters/btc-bridge';
+
+export {
+  ERC20Minter,
+  createActor as createERC20MinterActor
+} from './canisters/erc20-minter';
+
+export {
+  SignatureVerification,
+  createActor as createSignatureVerificationActor
+} from './canisters/signature-verification';
