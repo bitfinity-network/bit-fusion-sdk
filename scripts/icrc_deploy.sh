6a701f4176043c73640ae2b20bd91c23edcb92ee
Max <maximilian@live.co.uk>
2024-04-08 09:32:42 +0100

    deploy script

--- scripts/icrc_deploy.sh
+++ scripts/icrc_deploy.sh
@@ -1,12 +1,5 @@
-# Script to set up BTC bridge infrasturcture into local DFX replica and test BTC bridging flow
-# It uses dfx instead of dfx command as the current version of dfx (0.18) has a bug not allowing BTC operations to
-# work. To run the script download the dfx v0.17 from https://github.com/dfinity/sdk/releases and put it into the root
-# of the repo.
-#
-# For btc operations it uses bitcoin core. In Ubuntu this tool is ~/bitcoin-25.0/bin/bitcoin-cli and bitcoin-core.daemon, but on
-# other platforms it can be bitcoind and bitcoin.cli. Adjust accordingly. Before the script is run, the daemon must
-# be run:
-# bitcoin-core.daemon -conf=<path_to_config> -datadir=<path_to_data_dir>
+# Use latets vers
+
 set -e
 
 start_icx() {
@@ -25,19 +18,16 @@ CHAIN_ID=355113
 dfx stop
 dfx start --host 127.0.0.1:4943 --background --clean --enable-bitcoin 2> dfx_stderr.log
 
-dfx identity new --force btc-admin
-dfx identity use btc-admin
+dfx identity new --force icrc-admin
+dfx identity use icrc-admin
 
 ADMIN_PRINCIPAL=$(dfx identity get-principal)
 ADMIN_WALLET=$(dfx identity get-wallet)
 
-########## Deploy ckBTC canisters ##########
+########## Deploy  canisters ##########
 
 dfx canister create token2
 
-CKBTC_LEDGER=$(dfx canister id token)
-CKBTC_KYT=$(dfx canister id ic-ckbtc-kyt)
-CKBTC_MINTER=$(dfx canister id ic-ckbtc-minter)
 ICRC2_TOKEN=$(dfx canister id token2)
 
 USER_PRINCIPICAL="qhjy5-udjmu-rqh6d-abbcl-63l4x-gbwoi-ip5qu-vocyn-docsu-fideu-eae"
@@ -67,7 +57,7 @@ dfx deploy token2 --argument "(variant {Init = record {
     }
 }})"
 
-########## Deploy EVM, BTC bridge, ICRC2 Minter ##########
+########## Deploy EVM, ICRC2 Minter ##########
 
 dfx canister create evm_testnet
 dfx canister create icrc2-minter
@@ -108,46 +98,11 @@ dfx deploy spender --argument "(principal \"$ICRC2_MINTER\")"
 
 start_icx
 
-########## Deploy BFT and Token contracts ##########
+########## Deploy BFT and ICRC2 contracts ##########
 
 ETH_WALLET=$(cargo run -q -p create_bft_bridge_tool -- create-wallet --evm-canister="$EVM")
 ETH_WALLET_ADDRESS=$(cargo run -q -p create_bft_bridge_tool -- wallet-address --wallet="$ETH_WALLET")
 ETH_WALLET_CANDID=$(cargo run -q -p create_bft_bridge_tool -- wallet-address --wallet="$ETH_WALLET" --candid)
-
-BTC_BRIDGE=$(dfx canister id btc-bridge)
-
-res=$(dfx canister call btc-bridge get_evm_address)
-res=${res#*\"}
-BTC_BRIDGE_ECDSA_ADDRESS=${res%\"*}
-
-echo "BTC bridge ecdsa address: ${BTC_BRIDGE_ECDSA_ADDRESS}"
-
-echo "Minting ETH tokens for BTC bridge canister"
-dfx canister call evm_testnet mint_native_tokens "(\"${BTC_BRIDGE_ECDSA_ADDRESS}\", \"340282366920938463463374607431768211455\")"
-
-BTC_BRIDGE_CONTRACT_ADDRESS=$(cargo run -q -p create_bft_bridge_tool -- deploy-bft-bridge --minter-address="$BTC_BRIDGE_ECDSA_ADDRESS" --evm="$EVM" --wallet="$ETH_WALLET")
-echo "BTC bridge contract address: $BTC_BRIDGE_CONTRACT_ADDRESS"
-
-TOKEN_ETH_ADDRESS=$(cargo run -q -p create_bft_bridge_tool -- create-token \
-  --bft-bridge-address="$BTC_BRIDGE_CONTRACT_ADDRESS" \
-  --token-name=BTC \
-  --token-id="$CKBTC_LEDGER" \
-  --evm-canister="$EVM" \
-  --wallet="$ETH_WALLET")
-
-echo "Wrapped token ETH address: $TOKEN_ETH_ADDRESS" 
-
-echo "Configuring BTC bridge canister"
-dfx canister call btc-bridge admin_configure_bft_bridge "(record {
-  decimals = 0;
-  token_symbol = vec { 42; 54; 43; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; };
-  token_address = \"$TOKEN_ETH_ADDRESS\";
-  bridge_address = \"$BTC_BRIDGE_CONTRACT_ADDRESS\";
-  erc20_chain_id = 355113;
-  token_name = vec { 42; 54; 43; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0; };
-})"
-
-######### Deploy BFT Bridge and Token Contract for ICRC2 Minter ###########
 
 res=$(dfx canister call icrc2-minter get_minter_canister_evm_address)
 res=${res#*\"}
@@ -163,47 +118,4 @@ echo "ICRC2 bridge contract address: $ICRC2_BRIDGE_CONTRACT_ADDRESS"
 
 echo "Register bft bridge contract address with icrc2-minter"
 dfx canister call icrc2-minter register_evmc_bft_bridge "(\"$ICRC2_BRIDGE_CONTRACT_ADDRESS\")"
-
-ICRC2_WRAPPED_TOKEN_ADDRESS=$(cargo run -q -p create_bft_bridge_tool -- create-token \
-  --bft-bridge-address="$ICRC2_BRIDGE_CONTRACT_ADDRESS" \
-  --token-name="Aux Token" \
-  --token-id="$ICRC2_TOKEN" \
-  --evm-canister="$EVM" \
-  --wallet="$ETH_WALLET")
-
-echo "ICRC2 Wrapped token address: $ICRC2_WRAPPED_TOKEN_ADDRESS"
-
-########## Create BTC and move them into wrapped EVM token ##########
-
-r1=$(dfx canister call ic-ckbtc-minter get_btc_address "(record { owner = opt principal \"$BTC_BRIDGE\"; subaccount = opt $ETH_WALLET_CANDID; })")
-r2=${r1#*\"}
-address=${r2%\"*}
-
-echo Deposit BTC address: "$address"
-
-echo "Minting BTC block to ckBTC minter deposit address"
-~/bitcoin-25.0/bin/bitcoin-cli -conf="${PWD}/src/create_bft_bridge_tool/bitcoin.conf" generatetoaddress 1 "$address"
-
-# Wait to let ckBTC get the info about the new block
-sleep 10
-
-echo "Requesting minting wrapped tokens"
-dfx canister call btc-bridge btc_to_erc20 "\"$ETH_WALLET_ADDRESS\""
-
-# Wait for EVM to process the mint transaction
-sleep 5
-
-########## Burn wrapped token and receive BTC to the current wallet ##########
-
-BTC_ADDRESS=$(~/bitcoin-25.0/bin/bitcoin-cli -conf="${PWD}/src/create_bft_bridge_tool/bitcoin.conf" getnewaddress)
-
-cargo run -q -p create_bft_bridge_tool -- burn-wrapped \
-  --wallet="$ETH_WALLET" \
-  --evm-canister="$EVM" \
-  --bft-bridge="$BTC_BRIDGE_CONTRACT_ADDRESS" \
-  --token-address="$TOKEN_ETH_ADDRESS" \
-  --address="$BTC_ADDRESS" \
-  --amount=100000000
-
-sleep 5
-dfx canister call ic-ckbtc-minter retrieve_btc_status_v2_by_account "(opt record { owner = principal \"$BTC_BRIDGE\"; })"
+echo "Finished!!!!!"
