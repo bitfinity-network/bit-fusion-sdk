f2001c0b49e7b26a3ad731e347b2038f0b26932d
Alex Shelkovskiy <alexshelkov@gmail.com>
2024-04-09 14:00:21 +0600

    Btc fixes

--- js-client/src/btc.ts
+++ js-client/src/btc.ts
@@ -2,30 +2,26 @@ import { BaseConnector as BtcConnector } from '@particle-network/btc-connectkit'
 import { WalletClient as EthConnector } from 'viem';
 import { Principal } from '@dfinity/principal';
 
-import { createBtcBridgeActor, BtcBridgeActor } from './ic';
+import { BtcBridgeActor } from './ic';
 import { BTC_BRIDGE_CANISTER_ID } from './constants';
 import { ethAddrToSubaccount } from './utils';
 
 export class BtcBridge {
-  protected btcBridgeActor: typeof BtcBridgeActor;
-
   constructor(
     protected btc: BtcConnector,
     protected eth: EthConnector
-  ) {
-    this.btcBridgeActor = createBtcBridgeActor(BTC_BRIDGE_CANISTER_ID);
-  }
+  ) {}
 
   async bridgeBtc(satoshis: number) {
     const [ethAddress] = await this.eth.getAddresses();
 
-    const btcAddress = await this.btcBridgeActor.get_btc_address({
+    const btcAddress = await BtcBridgeActor.get_btc_address({
       owner: [Principal.fromText(BTC_BRIDGE_CANISTER_ID)],
       subaccount: [ethAddrToSubaccount(ethAddress)]
     });
 
     await this.btc.sendBitcoin(btcAddress, satoshis);
 
-    await this.btcBridgeActor.btc_to_erc20(ethAddress);
+    await BtcBridgeActor.btc_to_erc20(ethAddress);
   }
 }
