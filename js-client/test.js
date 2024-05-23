fe9f807b3e5747a2ab5c5be5d6f4598aeabdb9bf
Max <maximilian@live.co.uk>
2024-05-23 19:09:56 +0200

    changes to the code

--- js-client/test.js
+++
@@ -1,6 +0,0 @@
-const { createICRC1Actor } = require('./src/ic');
-
-(async () => {
-  const actor = createICRC1Actor('bkyz2-fmaaa-aaaaa-qaaaq-cai');
-  //   await actor.icrc1_decimals();
-})();
--- scripts/ord/test_ordinals_init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# Creates wallet and returns new address(other scripts require this address in `WALLET_ADDRESS` env, be sure to set with `export WALLET_ADDRESS=<ADDRESS>`).
-# Also this script mine 101 blocks to activate wallet.
-
-docker exec ord-cli ord --regtest --bitcoin-rpc-url bitcoind:18443 wallet --server-url http://ord:1338 create
-WALLET_ADDRESS=$(docker exec -it ord-cli ord --regtest --bitcoin-rpc-url bitcoind:18443 wallet --server-url http://ord:1338 receive | jq -r ".addresses[0]")
-docker exec -it bitcoind bitcoin-cli -rpcuser=user -rpcpassword=pass -regtest generatetoaddress 101 $WALLET_ADDRESS
-echo $WALLET_ADDRESS
