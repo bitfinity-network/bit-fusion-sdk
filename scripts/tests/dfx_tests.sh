6c51a8c44a501dc47c12173a52a8a0919bea6bea
veeso <christian.visintin@veeso.dev>
2024-06-25 10:05:03 +0200

    fix: script

--- scripts/tests/dfx_tests.sh
+++ scripts/tests/dfx_tests.sh
@@ -4,6 +4,14 @@ export ORD_BITCOIN_RPC_USERNAME=ic-btc-integration
 export ORD_BITCOIN_RPC_PASSWORD="QPQiNaph19FqUsCrBRN0FII7lyM26B51fAMeBQzCb-E="
 LOGFILE=./target/dfx_tests.log
 
+usage() {
+  echo "Usage: $0 [options]"
+  echo "Options:"
+  echo "  -h, --help                                      Display this help message"
+  echo "  --docker                                        Setup docker containers"
+  echo "  --github-ci                                     Use this flag when running in GitHub CI"
+}
+
 setup_docker() {
     PREV_PATH=$(pwd)
     cd btc-deploy/
@@ -19,23 +27,47 @@ stop_docker() {
 }
 
 WITH_DOCKER="0"
-if [ "$1" == "--docker" ]; then
-    WITH_DOCKER="1"
-    shift
-fi
+GITHUB_CI="0"
 
-# set dfxvm to use the correct version and use docker
-if [ "$1" == "--github-ci" ]; then
+ARGS=$(getopt -o h --long docker,github-ci,help -- "$@")
+while true; do
+  case "$1" in
+  --docker)
+    DOCKER="1"
     shift
-    WITH_DOCKER="1"
-    dfxvm default 0.16.1
+    ;;
+
+  --github-ci)
+    GITHUB_CI="1"
+    shift
+    ;;
+
+  -h | --help)
+    usage
+    exit 255
+    ;;
+
+  --)
+    shift
+    break
+    ;;
+
+  *)
+    break
+    ;;
+  esac
+done
+
+# set dfxvm to use the correct version
+if [ "$GITHUB_CI" -gt 0 ]; then
+    dfxvm default 0.18.0
 fi
 
 
 killall -9 icx-proxy || true
 dfx stop
 
-if [ "$WITH_DOCKER" -eq 1 ]; then
+if [ "$WITH_DOCKER" -gt 0 ]; then
     setup_docker
 fi
 
@@ -69,6 +101,6 @@ killall -9 icx-proxy || true
 
 dfx stop
 
-if [ "$WITH_DOCKER" -eq 1 ]; then
+if [ "$WITH_DOCKER" -gt 0 ]; then
     stop_docker
 fi
