82def35afcc610899e0e9496fe68c26a971aaa33
Francesco <ufoscout@gmail.com>
2024-07-12 08:13:57 +0200

    remove ci-wf

--- scripts/tests/dfx_tests.sh
+++ scripts/tests/dfx_tests.sh
@@ -97,26 +97,6 @@ dfx ledger fabricate-cycles --t 1000000 --canister $wallet_principal
 
 sleep 10
 
-BTC_ENDPOINT_READY=255
-BTC_ENDPOINT_ELAPSED=0
-BTC_ENDPOINT_SLEEP_SECONDS=5
-BTC_ENDPOINT_TIMEOUT_SECONDS=180
-
-while [ "$BTC_ENDPOINT_READY" -ne 0 ]; do
-  curl --verbose --user ${ORD_BITCOIN_RPC_USERNAME}:${ORD_BITCOIN_RPC_PASSWORD} --data-binary '{"jsonrpc":"1.0","id":"curltest","method":"getblockchaininfo","params":[]}' -H 'content-type:text/plain;' http://127.0.0.1:18443/
-  BTC_ENDPOINT_READY=$?
-  echo "BITCOIN READY $BTC_ENDPOINT_READY"
-  sleep $BTC_ENDPOINT_SLEEP_SECONDS
-  let BTC_ENDPOINT_ELAPSED=$BTC_ENDPOINT_ELAPSED+$BTC_ENDPOINT_SLEEP_SECONDS
-  if [ "$BTC_ENDPOINT_ELAPSED" -gt $BTC_ENDPOINT_TIMEOUT_SECONDS ]; then
-    echo "Bitcoin endpoint not ready after $BTC_ENDPOINT_TIMEOUT_SECONDS seconds; aborting"
-    cd btc-deploy/
-    docker compose logs bitcoind
-    cd -
-    exit 1
-  fi
-done
-
 cargo test -p integration-tests --features dfx_tests $@
 TEST_RESULT=$?
 
