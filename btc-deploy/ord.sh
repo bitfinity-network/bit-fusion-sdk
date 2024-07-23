76de2bcd066d62bd8c8f68d5ab133bd95136b6a9
veeso <christian.visintin@veeso.dev>
2024-07-23 18:53:02 +0200

    I will run 'terraform fmt' before committing.

--- btc-deploy/ord.sh
+++ btc-deploy/ord.sh
@@ -2,5 +2,5 @@
 
 echo "Ord version: $(./ord --version)"
 
-local-ssl-proxy --source 8001 --target 8000 --key localhost+3-key.pem --cert localhost+3.pem &
+local-ssl-proxy --source 8001 --target 8000 --key localhost+3-key.pem --cert localhost+3.crt &
 ./ord -r --index-runes --bitcoin-rpc-url=http://bitcoind:18443 server --http-port=8000
