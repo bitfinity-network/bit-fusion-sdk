4168d3c6910ce60c88eea66d1a24666aa09e8c6f
f3kilo <f3kilo@yandex.ru>
2024-08-08 08:47:59 +0300

    inspect message

---
+++ src/erc20-minter/src/inspect.rs
@@ -0,0 +1,27 @@
+use bridge_canister::bridge_inspect;
+use bridge_did::error::BftResult;
+use ic_exports::ic_cdk;
+use ic_exports::ic_cdk::{api, inspect_message};
+use ic_exports::ic_kit::ic;
+
+use crate::canister;
+
+#[inspect_message]
+async fn inspect_message() {
+    bridge_inspect();
+    let check_result = inspect_method(&api::call::method_name()).await;
+
+    if let Err(e) = check_result {
+        ic::trap(&format!("Call rejected by inspect check: {e:?}"));
+    } else {
+        api::call::accept_message();
+    }
+}
+
+async fn inspect_method(method: &str) -> BftResult<()> {
+    let config = canister::get_runtime_state().borrow().config.clone();
+    match method {
+        "set_base_bft_bridge_contract" => config.borrow().check_owner(ic::caller()),
+        _ => Ok(()),
+    }
+}
