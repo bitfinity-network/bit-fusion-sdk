f87c641d833bdc6871c2d82684b801c815703597
f3kilo <f3kilo@yandex.ru>
2024-08-19 09:25:07 +0300

    ctx

--- src/integration-tests/tests/context/bridge_ctx.rs
+++ src/integration-tests/tests/context/bridge_ctx.rs
@@ -1,26 +1,58 @@
 use std::collections::HashMap;
 
-use did::H160;
+use bridge_did::id256::Id256;
+use did::{H160, U256};
 
 use crate::context::TestContext;
 use crate::utils::error::Result;
 
-pub struct BridgeContextState<Ctx: BridgeTestContext> {
-    ctx: Ctx,
-    token_pairs: Vec<TokenPair<Ctx::BaseTokenId>>,
-    users: HashMap<Ctx::BaseUserId, UserState>,
-    bft_bridge: H160,
-    fee_charge: H160,
+pub struct BridgeTestConfig {
+    pub base_tokens_number: usize,
+    pub users_number: usize,
+    pub operations_per_user: usize,
 }
 
-pub struct UserState {}
+pub struct BridgeTestState<Ctx: BridgeTestContext> {
+    ctx: Ctx,
+    users: Vec<Ctx::UserId>,
+}
 
 pub struct TokenPair<BaseId> {
-    base: BaseId,
-    wrapped: H160,
+    pub base: BaseId,
+    pub wrapped: H160,
+}
+
+pub struct BridgeContracts {
+    pub bft_bridge_contract: H160,
+    pub fee_charge_contract: H160,
+}
+
+impl BridgeContracts {
+    pub async fn init(ctx: &impl TestContext) -> Result<Self> {
+        todo!()
+    }
 }
 
 pub trait BridgeTestContext: TestContext {
-    type BaseTokenId;
-    type BaseUserId;
+    type UserId;
+    type TokenId;
+
+    async fn new_base_token(&self) -> Result<Self::TokenId>;
+    async fn new_user(&self) -> Result<Self::UserId>;
+    async fn mint_base_token(&self, user: &Self::UserId, token: &Self::TokenId);
+    async fn deposit(
+        &self,
+        bridge: H160,
+        from: &Self::UserId,
+        to: H160,
+        amount: U256,
+    ) -> Result<U256>;
+
+    async fn withdraw(
+        &self,
+        bridge: H160,
+        from: &Self::UserId,
+        to: H160,
+        amount: U256,
+    ) -> Result<U256>;
 }
