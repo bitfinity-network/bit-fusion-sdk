12fcf57a16245f32e6fd9c9220094a3e198ddeee
Maxim <maxim@gritsenko.biz>
2024-08-26 15:27:09 +0900

    refactor: Remove DepositResult type

--- src/rune-bridge/src/core/mod.rs
+++ src/rune-bridge/src/core/mod.rs
@@ -1,30 +1,4 @@
-use bridge_did::order::SignedMintOrder;
-use candid::CandidType;
-use did::H256;
-use serde::Deserialize;
-
-use crate::rune_info::RuneName;
-
 pub mod deposit;
 pub mod index_provider;
 pub mod utxo_provider;
 pub mod withdrawal;
-
-#[derive(Debug, Clone, CandidType, Deserialize)]
-pub enum DepositResult {
-    MintOrderSigned {
-        mint_order: Box<SignedMintOrder>,
-        rune_name: RuneName,
-        amount: u128,
-    },
-    MintRequested {
-        tx_id: H256,
-        rune_name: RuneName,
-        amount: u128,
-    },
-    Minted {
-        tx_id: H256,
-        rune_name: RuneName,
-        amount: u128,
-    },
-}
