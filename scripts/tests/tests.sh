18f43aaccebc8711ace7609ac3f1ac69606d5e66
f3kilo <f3kilo@yandex.ru>
2024-08-30 11:57:40 +0300

    script changes reverted

--- scripts/tests/tests.sh
+++ scripts/tests/tests.sh
@@ -12,4 +12,4 @@ INTEGRATION_TESTS_TO_RUN=$1
 cargo test
 
 # WASM integration tests
-cargo test -p integration-tests --no-default-features --features "pocket_ic_integration_test" --features state_machine_tests -- $INTEGRATION_TESTS_TO_RUN --nocapture 
+cargo test -p integration-tests --no-default-features --features "pocket_ic_integration_test" --features state_machine_tests -- $INTEGRATION_TESTS_TO_RUN
