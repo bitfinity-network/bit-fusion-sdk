80aa0920bc9e5fc4ec58f6bdab7b3e821607d813
veeso <christian.visintin@veeso.dev>
2024-09-04 13:58:18 +0200

    fix: don't use any memory id below 100

--- src/brc20-bridge/src/memory.rs
+++ src/brc20-bridge/src/memory.rs
@@ -1,10 +1,10 @@
-//! Memory ids for bridge
+//! Memory IDs for the Bridge canister.
 //!
-//! DO NOT USE ANY MEMORY ID BELOW 10, since used by the sdk
+//! DO NOT USE MEMORY IDS BELOW 100, as they are reserved for sdk use.
 
 use ic_stable_structures::MemoryId;
 
-pub const CONFIG_MEMORY_ID: MemoryId = MemoryId::new(10);
-pub const MASTER_KEY_MEMORY_ID: MemoryId = MemoryId::new(12);
-pub const UNUSED_UTXOS_MEMORY_ID: MemoryId = MemoryId::new(13);
-pub const USED_UTXOS_MEMORY_ID: MemoryId = MemoryId::new(14);
+pub const CONFIG_MEMORY_ID: MemoryId = MemoryId::new(100);
+pub const MASTER_KEY_MEMORY_ID: MemoryId = MemoryId::new(101);
+pub const UNUSED_UTXOS_MEMORY_ID: MemoryId = MemoryId::new(102);
+pub const USED_UTXOS_MEMORY_ID: MemoryId = MemoryId::new(103);
--- src/btc-bridge/src/memory.rs
+++ src/btc-bridge/src/memory.rs
@@ -1,8 +1,8 @@
 //! Memory IDs for the BTC Bridge program.
 //!
-//! DO NOT USE MEMORY IDS BELOW 10, as they are reserved for sdk use.
+//! DO NOT USE MEMORY IDS BELOW 100, as they are reserved for sdk use.
 
 use ic_stable_structures::MemoryId;
 
-pub const BTC_CONFIG_MEMORY_ID: MemoryId = MemoryId::new(10);
-pub const WRAPPED_TOKEN_CONFIG_MEMORY_ID: MemoryId = MemoryId::new(11);
+pub const BTC_CONFIG_MEMORY_ID: MemoryId = MemoryId::new(100);
+pub const WRAPPED_TOKEN_CONFIG_MEMORY_ID: MemoryId = MemoryId::new(101);
