61c7066dc32c47e080f520179d032b49aa5e9355
Yasir <yasir@shariff.dev>
2024-09-06 10:47:07 +0300

    chore: fix clippy warn

--- src/integration-tests/tests/context/mod.rs
+++ src/integration-tests/tests/context/mod.rs
@@ -108,7 +108,7 @@ pub trait TestContext {
     /// Sends tx with notification to EVMc.
     async fn send_notification_tx(
         &self,
-        user: &Wallet<SigningKey>,
+        user: &Wallet<'_, SigningKey>,
         input: NotificationInput,
     ) -> Result<H256> {
         let address: H160 = user.address().into();
@@ -257,8 +257,9 @@ pub trait TestContext {
             .await?;
 
         let raw_client = self.client(self.canisters().icrc2_bridge(), self.admin_name());
+
         raw_client
-            .update("set_bft_bridge_contract", (bridge_address.clone(),))
+            .update::<_, ()>("set_bft_bridge_contract", (bridge_address.clone(),))
             .await?;
 
         Ok(bridge_address)
