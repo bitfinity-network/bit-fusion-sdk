b5da85c888bb91b1910a7d06b3ace4e4dee1e2c7
Francesco <ufoscout@gmail.com>
2024-09-13 10:52:06 +0200

    Fix rune pocket ic test

--- src/integration-tests/tests/pocket_ic_integration_test/rune.rs
+++ src/integration-tests/tests/pocket_ic_integration_test/rune.rs
@@ -6,11 +6,9 @@ use bridge_did::init::{BridgeInitData, RuneBridgeConfig};
 use candid::Principal;
 use did::H160;
 use eth_signer::sign_strategy::{SigningKeyId, SigningStrategy};
-use ic_canister_client::{CanisterClient, PocketIcClient};
 use ic_exports::ic_cdk::api::management_canister::bitcoin::BitcoinNetwork;
 use ic_exports::ic_kit::mock_principals::bob;
-use ic_management_canister_types::{EcdsaCurve, EcdsaKeyId};
-use rune_bridge::interface::GetAddressError;
+
 
 use crate::context::TestContext;
 use crate::utils::wasm::get_rune_bridge_canister_bytecode;
@@ -22,7 +20,6 @@ const KEY_ID: &str = "test_key_1";
 struct RunesSetup {
     ctx: PocketIcTestContext,
     rune_bridge: Principal,
-    rune_bridge_client: RuneBridgeClient<PocketIcClient>,
 }
 
 impl RunesSetup {
@@ -61,7 +58,6 @@ impl RunesSetup {
         RunesSetup {
             ctx: context,
             rune_bridge: bridge,
-            rune_bridge_client
         }
     }
 
@@ -69,25 +65,29 @@ impl RunesSetup {
 
 #[tokio::test]
 async fn generates_correct_deposit_address() {
+    
     const ETH_ADDRESS: &str = "0x4e37fc8684e0f7ad6a6c1178855450294a16b418";
     let eth_address = H160::from_hex_str(ETH_ADDRESS).unwrap();
-
+    
     let setup = RunesSetup::init().await;
-    let address = setup.rune_bridge_client.get_deposit_address(&eth_address).await.unwrap().unwrap();
+    let rune_bridge_client = RuneBridgeClient::new(setup.ctx.client(setup.rune_bridge, "alice"));
+    let address = rune_bridge_client.get_deposit_address(&eth_address).await.unwrap().unwrap();
+
+    let expected = "bc1qq9c8dh2w7vk25644y3ulf808vyggx9z8c6tapp".to_string();
 
     assert_eq!(
         address,
-        "bc1qdmwl446fszfj40wpup4dgq6ezv8l6ajhs2zxyz".to_string()
+        expected
     );
 
     const ANOTHER_ETH_ADDRESS: &str = "0x4e37fc8684e0f7ad6a6c1178855450294a16b419";
     let eth_address = H160::from_hex_str(ANOTHER_ETH_ADDRESS).unwrap();
 
-    let address = setup.rune_bridge_client.get_deposit_address(&eth_address).await.unwrap().unwrap();
+    let address = rune_bridge_client.get_deposit_address(&eth_address).await.unwrap().unwrap();
 
     assert_ne!(
         address,
-        "bc1qdmwl446fszfj40wpup4dgq6ezv8l6ajhs2zxyz".to_string()
+        expected
     );
 
 }
