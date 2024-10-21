afa47b68fcea9b82323f8b3b3a8eb5ab0bb505f3
f3kilo <f3kilo@yandex.ru>
2024-10-21 10:33:17 +0300

    btc events handler

--- src/btc-bridge/src/ops/events_handler.rs
+++ src/btc-bridge/src/ops/events_handler.rs
@@ -1,3 +1,4 @@
+use bridge_canister::bridge::OperationAction;
 use bridge_canister::runtime::service::fetch_logs::BftBridgeEventHandler;
 use bridge_canister::runtime::RuntimeState;
 use bridge_did::error::{BftResult, Error};
@@ -7,64 +8,41 @@ use bridge_did::reason::BtcDeposit;
 use candid::Decode;
 
 use super::BtcBridgeOpImpl;
-use crate::canister::SharedRuntime;
 
-pub struct BtcEventsHandler {
-    runtime: SharedRuntime,
-}
+pub struct BtcEventsHandler;
 
-impl BtcEventsHandler {
-    pub fn new(runtime: SharedRuntime) -> Self {
-        Self { runtime }
-    }
-
-    fn state(&self) -> RuntimeState<BtcBridgeOpImpl> {
-        self.runtime.borrow().state().clone()
-    }
-}
-
-impl BftBridgeEventHandler for BtcEventsHandler {
-    fn on_wrapped_token_minted(&self, event: MintedEventData) -> BftResult<()> {
+impl BftBridgeEventHandler<BtcBridgeOpImpl> for BtcEventsHandler {
+    fn on_wrapped_token_minted(
+        &self,
+        event: MintedEventData,
+    ) -> Option<OperationAction<BtcBridgeOpImpl>> {
         let nonce = event.nonce;
-        let dst_address = event.recipient.clone();
-        let op = BtcBridgeOpImpl(BtcBridgeOp::Erc20MintConfirmed(event));
+        let update_to = BtcBridgeOpImpl(BtcBridgeOp::Erc20MintConfirmed(event));
 
-        self.state()
-            .borrow_mut()
-            .operations
-            .update_by_nonce(&dst_address, nonce, op);
-
-        Ok(())
+        Some(OperationAction::Update { nonce, update_to })
     }
 
-    fn on_wrapped_token_burnt(&self, event: BurntEventData) -> BftResult<()> {
+    fn on_wrapped_token_burnt(
+        &self,
+        event: BurntEventData,
+    ) -> Option<OperationAction<BtcBridgeOpImpl>> {
         log::trace!("wrapped token burnt");
         let memo = event.memo();
         let op = BtcBridgeOpImpl(BtcBridgeOp::WithdrawBtc(event));
-
-        let op_id = self
-            .state()
-            .borrow_mut()
-            .operations
-            .new_operation(op.clone(), memo);
-        self.runtime.borrow().schedule_operation(op_id, op);
-        Ok(())
+        Some(OperationAction::Create(op, memo))
     }
 
-    fn on_minter_notification(&self, event: NotifyMinterEventData) -> BftResult<()> {
+    fn on_minter_notification(
+        &self,
+        event: NotifyMinterEventData,
+    ) -> Option<OperationAction<BtcBridgeOpImpl>> {
         log::debug!("on_minter_notification {event:?}");
-
-        if let Some(operation_id) = event.try_decode_reschedule_operation_id() {
-            self.runtime.borrow().reschedule_operation(operation_id);
-            return Ok(());
-        }
 
         let mut btc_deposit = match Decode!(&event.user_data, BtcDeposit) {
             Ok(icrc_burn) => icrc_burn,
             Err(e) => {
-                let msg = format!("failed to decode BftBridge notification into BtcDeposit: {e}");
-                log::warn!("{msg}");
-                return Err(Error::Serialization(msg));
+                log::warn!("failed to decode BftBridge notification into BtcDeposit: {e}");
+                return None;
             }
         };
 
@@ -78,13 +56,6 @@ impl BftBridgeEventHandler for BtcEventsHandler {
         let op = BtcBridgeOpImpl(BtcBridgeOp::UpdateCkBtcBalance {
             eth_address: btc_deposit.recipient,
         });
-        let op_id = self
-            .state()
-            .borrow_mut()
-            .operations
-            .new_operation(op.clone(), memo);
-        self.runtime.borrow().schedule_operation(op_id, op);
-
-        Ok(())
+        Some(OperationAction::Create(op, memo))
     }
 }
